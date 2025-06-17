22a40fb05eb8bc0f5593fd9fc27e098b08d90883
pedrin-dev <pedrosiqueira@1995gmail.com>
2025-06-16 22:09:05 -0300

    Feat: Sutup do react e limpeza de arquivos boilerplate.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+       <main className="App-main">
+        
+      </main>
+       <footer className="App-footer">
+        
+      </footer>
+    </div>
+  );
+}
+
+export default App;
